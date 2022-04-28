60feb510910e72584a786f50f25b573fbd474d1f
Louis Moresi <louis.moresi@anu.edu.au>
2022-04-28 20:57:12 +1000

    Merge branch 'main' of https://github.com/ANU-RSES-Education/EMSC-4033-2022-assignment

--- User1/src/my_functions.py
+++ User1/src/my_functions.py
@@ -16,18 +16,26 @@ def my_documentation():
     
 ## Moderni progressio programmandi 
 
-Moderni programmandi rationem habet organicam, accumsan sentientem, ut mirum inveniat. In hoc cursu discimus quomodo per laminis perlegere discamus quomodo programmata aedificent et quomodo nostra edificent. Incipiamus cum aliquibus praecipuis quaestionibus. 
+Moderni programmandi rationem habet organicam, accumsan sentientem, ut mirum inveniat. 
+In hoc cursu discimus quomodo per laminis perlegere discamus quomodo programmata aedificent et 
+quomodo nostra edificent. Incipiamus cum aliquibus praecipuis quaestionibus. 
 
 ## Quid computatorium ? 
 
-Computatorium classicum est machina alicuius generis ad informationes puras expediendas. Varia elementa opus sunt ad hoc possibilis efficiendum, inter quas aliqua instrumenta initialisendi et recondendi informationes ante et post discursum est, et ma- china ad informationes expediendas (including casus ubi processus pendet ab ipsa informatione). Multae variae machinis his criteriis occurrere possunt, sed plerumque unam saltem exigentiam adiungimus:
+Computatorium classicum est machina alicuius generis ad informationes 
+puras expediendas. Varia elementa opus sunt ad hoc possibilis efficiendum, 
+inter quas aliqua instrumenta initialisendi et recondendi informationes ante 
+et post discursum est, et ma- china ad informationes expediendas (including casus 
+ubi processus pendet ab ipsa informatione). Multae variae machinis his criteriis 
+occurrere possunt, sed plerumque unam saltem exigentiam adiungimus:
 
 ## Aequationes mathematicae 
 
 Per "Navier-Stokes" datae sunt
 
 $$
-    \\frac{D \\mathbf{u}}{Dt} -\\nabla \cdot \\eta \\left( \\nabla \mathbf{u} + \\nabla \mathbf{u}^T \\right) - \\nabla p = \cdots
+    \\frac{D \\mathbf{u}}{Dt} -\\nabla \cdot \\eta \\left( \\nabla \mathbf{u} + 
+    \\nabla \mathbf{u}^T \\right) - \\nabla p = \cdots
 $$
 
 
